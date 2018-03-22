47f3a195703b056aa25ce78af7db8b1f470b2b34
Magda Balun <magi@op.pl>
2018-03-22 12:58:39 +0100

    Ceate index.js file

---
+++ index.js
@@ -0,0 +1,34 @@
+const formatDate = (timeInSeconds) => {
+
+	var hour = Math.floor(timeInSeconds / 3600);
+ 	var min = Math.floor((timeInSeconds % 3600) / 60);
+ 	var sec = Math.floor(timeInSeconds % 60);
+
+	
+	if (timeInSeconds <= 60) {
+		return sec + "s";
+	} else if (timeInSeconds === undefined) {
+		return `0s`;
+
+	} else if (timeInSeconds > 60 && timeInSeconds < 3600) {
+		if ( sec === 0 ) {
+			return min + "m";
+		}
+		return min + "m " + sec + "s";
+
+	} else if (timeInSeconds >= 3600) {
+		if ( min === 0 && sec === 0) {
+			return hour + "h";
+		} else if ( sec === 0 ) {
+			return hour + "h " + min + "m";
+		} else if ( min === 0 ) {
+			return hour + "h " + sec + "s";
+		}
+		return hour + "h " + min + "m " + sec + "s";
+
+	} 
+}
+
+formatDate();
+
+module.exports = formatDate;
